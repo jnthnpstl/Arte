61ac26b220f4583d3f0c65e3afcb657fba65ace4
jnthnpstl <111412026+dev-Jonathan@users.noreply.github.com>
2023-03-04 14:21:07 +0800

    "finished the tooltip and added item"

--- js/animation.js
+++ js/animation.js
@@ -1,17 +1,19 @@
+console.log("Printed");
+
 window.onscroll = function () {
   myFunction();
 };
 
 function myFunction() {
   if (document.documentElement.scrollTop > 50) {
-    document.querySelector(".brand-name > span:first-child").style =
+    document.querySelector(".brand-name a > span:first-child").style =
       "font-size : 24px";
-    document.querySelector(".brand-name > span:last-child").style =
+    document.querySelector(".brand-name a > span:last-child").style =
       "font-size : 14px";
   } else {
-    document.querySelector(".brand-name > span:first-child").style =
+    document.querySelector(".brand-name a > span:first-child").style =
       "font-size : 34px";
-    document.querySelector(".brand-name > span:last-child").style =
+    document.querySelector(".brand-name a > span:last-child").style =
       "font-size : 21px";
   }
 }
