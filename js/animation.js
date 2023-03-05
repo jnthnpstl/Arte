a865b7559f7c40775ec7765f737f46c5a67d8f35
jnthnpstl <111412026+dev-Jonathan@users.noreply.github.com>
2023-03-05 11:03:41 +0800

    "finished item nav feature"

--- js/animation.js
+++ js/animation.js
@@ -17,3 +17,50 @@ function myFunction() {
       "font-size : 21px";
   }
 }
+
+function fadeOut(params) {
+  const outterEl = document.querySelector(".paper .item");
+  const el = params;
+  viewItem(el);
+  outterEl.style = "transform: translateX(0px)";
+  outterEl.style.opacity = 1;
+  el.parentNode.style = "transform: translateX(50px)";
+  el.parentNode.style.opacity = 0;
+  el.parentNode.style.visibility = "hidden";
+}
+
+function viewItem(params) {
+  let ul = document.querySelector(".outer-paper-products ul");
+  if (ul.children.length >= 5) {
+    return;
+  }
+  let itemText = params.children[1].innerHTML;
+  let itemPrice = params.children[2].innerHTML;
+  let itemDetail = document.querySelector(".inner-outer-products .item-detail");
+  itemDetail.children[1].innerHTML = itemPrice;
+  itemDetail.children[0].innerHTML = itemText;
+  let newLi = document.createElement("li");
+  let newSpan = document.createElement("span");
+
+  newLi.innerHTML = itemText;
+  newSpan.innerHTML = "❯";
+  ul.appendChild(newSpan);
+  ul.appendChild(newLi);
+}
+
+function returnToPaper() {
+  let ul = document.querySelector(".outer-paper-products ul");
+  if (ul.children.length <= 4) {
+    return;
+  }
+  const paperProducts = document.querySelector(".paper .paper-products");
+  const outterEl = document.querySelector(".paper .item");
+  let len = ul.children.length;
+  ul.children[len - 1].remove();
+  ul.children[len - 2].remove();
+  paperProducts.style = "transform: translateX(0px)";
+  paperProducts.style.opacity = 1;
+  paperProducts.style.display = "grid";
+  outterEl.style = "transform: translateX(-50px)";
+  outterEl.style.visibility = "hidden";
+}
